906fb2a2f34900dc762625050339c6c411d8de5f
Deivid Fortuna <deividfortuna@gmail.com>
2026-09-16 11:31:06 +1200

    Log the detected package manager

--- src/main.ts
+++ src/main.ts
@@ -56,6 +56,7 @@ export function run(): void {
       core.setOutput('node-version-file', '')
     }
 
+    core.info("Detected package manager: " + packageManager);
     core.setOutput('package-manager', packageManager)
     core.setOutput(
       'lockfile',
